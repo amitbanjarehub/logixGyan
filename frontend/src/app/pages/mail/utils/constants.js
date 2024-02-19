4fb3ce4003dfbad3debb348e41dfb70483c265c9
amitbanjarehub <151738339+amitbanjarehub@users.noreply.github.com>
2024-02-19 10:02:45 +0530

    Delete frontend/src/app/pages/mail directory

--- frontend/src/app/pages/mail/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const MAIL_APP_ACTIONS = {
-    SET_SELECTED_ITEMS: "set-selected-items",
-    SET_MAILS_LIST_REFRESH: "set-mails-list-refresh",
-    SET_LABELS_LIST_REFRESH: "set-labels-list-refresh",
-};
